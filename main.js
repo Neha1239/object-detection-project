9438aaacc6766bb6572d047f42b524ba55e313ad
Neha1239 <86868752+Neha1239@users.noreply.github.com>
2022-05-06 08:56:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function navigateAC() {
+    window.location.href='AC.html';
+}
+function navigateBedroom() {
+    window.location.href='Bedroom.html';
+}
+function navigateFan() {
+    window.location.href='Fan.html';
+}
+function navigateKitchen() {
+    window.location.href='Kitchen.html';
+}
+function navigateTV() {
+    window.location.href='TV.html';
+}
+function navigateWardrobe() {
+    window.location.href='Wardrobe.html';
+}
